19741089b660312867b416a54f50d3fa612a9356
theabhishek07 <asharma0269@gmail.com>
2020-04-22 00:42:16 +0530

    concates.java

---
+++ concates.java
@@ -0,0 +1,9 @@
+
+public class concates {
+    public static void main(String[] args) {
+        String s="Abhishek";
+        s=s.concat("Sharma");//s is also reffred to gaurav jain
+        System.out.println(s);
+    }
+    
+}
