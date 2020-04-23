a33b258b5ac824ca0df8cec69e3a209a609de5d1
theabhishek07 <asharma0269@gmail.com>
2020-04-23 20:26:22 +0530

    for_loop.java

---
+++ for_loop.java
@@ -0,0 +1,11 @@
+public class for_loop {
+    public static void main(String[] args) {
+        int a = 7;
+        for(int i=1 ;i<a; i++){
+        for(int j=a; j>=i;j--){
+                System.out.println("@");
+            }
+        System.out.println();
+    }
+    }
+}
