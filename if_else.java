ea1a46da7277162cf84a6291a46cbc8ea14051ef
theabhishek07 <asharma0269@gmail.com>
2020-04-24 17:51:07 +0530

    if.java

---
+++ if_else.java
@@ -0,0 +1,19 @@
+
+public class if_else {
+    public static void main(String[] args) {
+    int weight=56;
+    int age=21;
+    if(age>18){
+    if(weight>50){
+        System.out.println("you have donated blood");
+    }
+    else{
+        System.out.println("you are under weight");
+    }
+}
+    else{
+        System.out.println("you are underage");
+    
+}
+}
+}
