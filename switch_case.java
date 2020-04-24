876847aa7f577112e9d4e5579fc8f460982a106c
theabhishek07 <asharma0269@gmail.com>
2020-04-24 17:56:38 +0530

    switch_case.java

---
+++ switch_case.java
@@ -0,0 +1,13 @@
+public class switch_case {
+    public static void main(String[] args) {
+        int number = 16;
+        switch(number){
+            case 10: System.out.println("value is 10");
+            break ;
+            case 16: System.out.println("value is 16");
+            break;
+            default: System.out.println("not any number from above");
+        }
+    }
+    
+}
