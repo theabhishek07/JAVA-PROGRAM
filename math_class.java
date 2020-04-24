cc8af22e0e3a284711566a34f48a7e78b7f4c760
theabhishek07 <asharma0269@gmail.com>
2020-04-24 17:55:45 +0530

    math_class.java

---
+++ math_class.java
@@ -0,0 +1,15 @@
+public class math_class {
+    public static void main(String[] args) {
+        double x=28;
+        double y=4;
+               System.out.println("maximum of two numbers"+ Math.max(x, y));//return the max of two number
+               System.out.println("square root of y"+ Math.sqrt(y));//return the square root
+               System.out.println("power of x and y"+ Math.pow(x, y));//return 28^4= 28*28*28*28
+               System.out.println("logrithm of x "+ Math.log(x));//return log value
+               System.out.println("logrithm of y"+ Math.log(y));
+               System.out.println("log10 of x"+ Math.log(x));//return log value with base 10
+               System.out.println("log10 of y"+ Math.log(y));
+                        
+    }
+ 
+}
