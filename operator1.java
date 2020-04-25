1e617ad971e5744ea22e4ad94bbe9e449b019337
theabhishek07 <asharma0269@gmail.com>
2020-04-25 23:09:08 +0530

    operator1.java

---
+++ operator1.java
@@ -0,0 +1,9 @@
+public class shift_operator {
+    public static void main(String[] args) {
+        System.out.println(10<<3);//left shift(10*2^3)
+        System.out.println(10<<2);
+        System.out.println(10>>3);//right shit(10/2^3)
+        System.out.println(10>>2);
+    }
+    
+}
