aeec01cf4cabdc1167ae5f1f805d1a038afd91c4
theabhishek07 <asharma0269@gmail.com>
2020-04-26 00:36:03 +0530

    super1.java

---
+++ super1.java
@@ -0,0 +1,19 @@
+class fun{
+    void function1(){
+        System.out.println("Hello");
+    }
+}
+public class super1 extends fun {
+    void function2 (){
+        System.out.println("World");
+    }
+    void function3(){
+        super.function1();
+        
+    }
+    public static void main(String[] args) {
+        super1 obj =new super1();
+        obj.function3();
+        
+    }
+}
