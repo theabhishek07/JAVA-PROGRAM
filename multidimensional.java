c7ffee9168377a7b76ff47f8a4d7b310bbca4bb2
theabhishek07 <asharma0269@gmail.com>
2020-04-26 00:31:38 +0530

    multidimensional.java

---
+++ multidimensional.java
@@ -0,0 +1,13 @@
+public class multidimensional {
+    public static void main(String[] args) {
+    int arr[][]={{1,2,3},{3,4,5},{4,5,6}};
+    for(int i=0;i<3;i++){
+        for(int j=0; j<3;j++){
+        System.out.println(arr[i][j] + " ");
+        
+    }
+        System.out.println();
+    }
+        
+    
+}}
