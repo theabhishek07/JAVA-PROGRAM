7e63ee2f4022ac64a6a919be5a47a771baada49d
theabhishek07 <asharma0269@gmail.com>
2020-04-25 23:08:34 +0530

    operator.java

---
+++ operator.java
@@ -0,0 +1,10 @@
+public class operator {
+    public static void main(String[] args) {
+        int a = 10 , b = 20 , c = 15 , d = 12 ;
+        System.out.println(++a);// pre-increment
+         System.out.println(b++);// post-increment
+          System.out.println(--c);// pre-decrement
+           System.out.println(d--);// post-decrement
+    }
+    
+}
