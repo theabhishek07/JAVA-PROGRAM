56f9c8e7158120463907993184339a3c8e752701
theabhishek07 <asharma0269@gmail.com>
2020-04-27 00:11:41 +0530

    variables.java

---
+++ variables_example.java
@@ -0,0 +1,17 @@
+//class A{  
+//int data=10;(instance variable)  
+//static int m=200;(static variable)  
+//void method(){  
+//int n=30;(local variable ) 
+//}    
+
+
+public class variables_1 {
+    public static void main(String args[]){
+    //add two numbers
+    int a=1;
+    int b=15;
+    int c=a+b;
+    System.out.println(c);
+    }
+}
