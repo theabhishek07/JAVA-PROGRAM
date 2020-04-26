80774078cb755cce7f562f32eaca9f41319e3099
theabhishek07 <asharma0269@gmail.com>
2020-04-27 00:12:38 +0530

    throw_exception.java

---
+++ throw_example.java
@@ -0,0 +1,14 @@
+public class throw_example {
+    static void testAge(int n){
+        if(n<18)
+            throw new ArithmeticException("less age");
+        else
+            System.out.println("18 years boy");
+    }
+    public static void main(String[] args) {
+        testAge(17);
+        System.out.println("rest of code");
+        
+    }
+    
+}
