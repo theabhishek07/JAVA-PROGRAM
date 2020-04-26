f88b256270fad57b7dba671065afa9f6d6d537f4
theabhishek07 <asharma0269@gmail.com>
2020-04-27 00:10:04 +0530

    string.java

---
+++ string.java
@@ -0,0 +1,9 @@
+//s will only print abhishek and not concat sharma because s is only reffred to string abhishek not on abhishek sharma
+public class string {
+    public static void main(String[] args) {
+        String s="abhishek";
+        s.concat("sharma");
+        System.out.println(s);
+    }
+    
+}
