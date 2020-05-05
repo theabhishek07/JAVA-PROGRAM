a94f0ea36b0688f3d6c47335188a81489c4c584c
theabhishek07 <asharma0269@gmail.com>
2020-05-05 15:06:18 +0530

    input_output#6

---
+++ input_output#6.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class reader_example {
+    public static void main(String[] args) {
+        try{
+            Reader reader=new FileReader("C:\\Users\\ABHISHEK\\Documents\\NetBeansProjects\\practice\\target\\classes\\practice_java\\test.txt");
+            int data=reader.read();
+            while(data!=-1){
+                 System.out.println((char)data);
+                data=reader.read();
+                        
+                
+            }
+            reader.close();
+            
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+}
