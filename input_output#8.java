9544eb0fdd95da6e7815565919dbbc1770e099a6
theabhishek07 <asharma0269@gmail.com>
2020-05-05 15:06:53 +0530

    input_output#8

---
+++ input_output#8.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class buffered_writer {
+    public static void main(String[] args) throws Exception{
+        FileWriter writerss=new FileWriter("C:\\Users\\ABHISHEK\\Documents\\NetBeansProjects\\practice\\target\\classes\\practice_java\\test.txt");
+        BufferedWriter buff=new BufferedWriter(writerss);
+        buff.write("HELLO CODERS");
+        buff.close();
+        System.out.println("done");
+    }   
+}
