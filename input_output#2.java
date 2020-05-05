059b1870dd05f47179413504eec940e460e6f1ad
theabhishek07 <asharma0269@gmail.com>
2020-05-05 15:04:40 +0530

    input_output#2

---
+++ input_output#2.java
@@ -0,0 +1,17 @@
+import java.io.FileInputStream;
+import java.io.IOException; 
+public class file_input {
+    public static void main(String[] args) {
+        try{
+        FileInputStream fin= new FileInputStream("C:\\Users\\ABHISHEK\\Documents\\NetBeansProjects\\practice\\target\\classes\\practice_java\\test.txt");
+        int i=0;
+         while((i=fin.read())!=-1){
+            System.out.println((char)i);
+            }
+        fin.close();}
+            catch( IOException e){
+                    System.out.println(e);
+                    }
+        
+    }
+}
