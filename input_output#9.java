71c58892696fcfb0bdf05ab7e4f71f5248c7a920
theabhishek07 <asharma0269@gmail.com>
2020-05-05 15:07:09 +0530

    input_output#9

---
+++ input_output#9.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class buffered_reader {
+    public static void main(String[] args) throws Exception {
+        FileReader rd=new FileReader("C:\\Users\\ABHISHEK\\Documents\\NetBeansProjects\\practice\\target\\classes\\practice_java\\test.txt");
+        BufferedReader br= new BufferedReader(rd);
+         int i;
+         while((i=br.read())!=-1){//read the text file till reaches end of file i.e -1
+        System.out.println((char)i);
+    }
+         br.close();
+         rd.close();
+        
+    }
+    
+}
