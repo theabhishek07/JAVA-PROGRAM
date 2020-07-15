e894eea15d183c6625578c11654e45c1446f18b6
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-07-15 23:56:07 +0530

    Update custom_exception.java

--- custom_exception.java
+++ custom_exception.java
@@ -1,18 +1,22 @@
-class myException extends Exception{
-    public myException(String s){
+class myException extends Exception
+{
+    public myException(String s)
+    {
         super(s);
     }
 }
-public class custom_exception {
-    public static void main(String[] args) {
-        try{
+public class custom_exception 
+{
+    public static void main(String[] args)
+    {
+        try
+        {
             throw new myException("exceptionsss");
         }
-        catch(myException ex){
+        catch(myException ex)
+        {
             System.out.println("caught");
                System.out.println(ex.getMessage());
-        }
-       
-    }
-    
+        }  
+    } 
 }
