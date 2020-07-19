ef4dbf904185fb9a78372e37e63ef2ee97b48924
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-07-19 23:32:50 +0530

    Update while_loop.java

--- while_loop.java
+++ while_loop.java
@@ -1,9 +1,11 @@
-public class While_loop {  
-public static void main(String[] args) {  
+public class While_loop 
+{  
+public static void main(String[] args) 
+{  
     int i=1;  
     while(i<=10){  
         System.out.println(i);  
     i++;  
     }  
-}  
+  }  
 }  
