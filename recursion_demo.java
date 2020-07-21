903d555c86914ff8d6a8092f3641177a7f80e8e8
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-07-21 23:58:42 +0530

    Update recursion_demo.java

--- recursion_demo.java
+++ recursion_demo.java
@@ -1,11 +1,14 @@
-public class recursion_demo {
-   static int factorial(int n){
+public class recursion_demo
+{
+   static int factorial(int n)
+   {
         if(n==1)
             return 1;
         else
             return(n*factorial(n-1));
     }
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("factorial of number is" + factorial(5));
     }
     
