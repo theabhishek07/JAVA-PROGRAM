acfd9d229fc0bbe1a15e07d4e927defce46a5278
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-07-24 00:04:37 +0530

    Update checked_propogation.java

--- checked_propogation.java
+++ checked_propogation.java
@@ -1,20 +1,25 @@
 import java.io.IOException;
-public class checked_propogation {
-    void fun1() throws IOException{
+public class checked_propogation 
+{
+    void fun1() throws IOException
+    {
         throw new  IOException("error");
     }
-    void fun2() throws IOException{
+    void fun2() throws IOException
+    {
         fun1(); //exception not handled
         //exception propogated to fun3()
     }
-    void fun3(){
-        try{
+    void fun3()
+    {
+        try
+        {
             fun2();//exception handled
         }
         catch(Exception e){System.out.println("Exception handled");
     }}
-    
-      public static void main(String args[]){
+      public static void main(String args[])
+      {
           exception_prpogation obj =new exception_prpogation();
           obj.fun3();
           System.out.println("rest of the code");
