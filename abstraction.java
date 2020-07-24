858954bf830d1bca95339ddd9827f10f004fd727
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-07-25 01:13:54 +0530

    Update abstraction.java

--- abstraction.java
+++ abstraction.java
@@ -1,19 +1,25 @@
-abstract class shape{
+abstract class shape
+{
 abstract void draw();
 }
-class rectangle extends shape{
-    void draw(){
+class rectangle extends shape
+{
+    void draw()
+    {
         System.out.println("rectanle is drawn");
     }
 }
 class circle extends shape{
-    void draw(){
+    void draw()
+    {
         System.out.println("circle is drawn");
     }
 }
 
-public class abstraction {
-    public static void main(String[] args) {
+public class abstraction
+{
+    public static void main(String[] args) 
+    {
         shape s1=new circle();
         s1.draw();
     }
