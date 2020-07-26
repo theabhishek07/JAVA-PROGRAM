96c6b5c12d9f7897739a06315fead3cca674e61f
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-07-26 23:43:26 +0530

    Update basic_syntax.java

--- basic_syntax.java
+++ basic_syntax.java
@@ -1,7 +1,7 @@
-public class main {
-    public static void main (String args[]){
+public class main 
+{
+    public static void main (String args[])
+    {
         System.out.println("hello java");
-    }
-            
-    
+    } 
 }
