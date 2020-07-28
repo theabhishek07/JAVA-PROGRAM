0e136d6d8b8f6783c9425c66cfe9c3cc74d86133
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-07-28 22:42:13 +0530

    Update do_while.java

--- do_while.java
+++ do_while.java
@@ -1,9 +1,13 @@
-public class do_while {  
-public static void main(String[] args) {  
+public class do_while 
+{  
+public static void main(String[] args) 
+{  
     int i=1;  
-    do{  
+    do
+    {  
         System.out.println(i);  
     i++;  
-    }while(i<=10);  
+    }
+    while(i<=10);  
 }  
 }  
