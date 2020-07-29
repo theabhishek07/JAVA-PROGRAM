b5e1bc1cabfb8cb62891acf4773d9c3c8c2614fa
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-07-29 22:51:29 +0530

    Update exception_prpogation.java

--- exception_prpogation.java
+++ exception_prpogation.java
@@ -1,22 +1,30 @@
-public class exception_prpogation {
-    void fun1(){
+public class exception_prpogation
+{
+    void fun1()
+    {
         int data=50/0; //exception occurs 
     }
     //exception prpogated to fun2()
-    void fun2(){
+    void fun2()
+    {
         fun1(); //exception not handled
         //exception propogated to fun3()
     }
-    void fun3(){
-        try{
+    void fun3()
+    {
+        try
+        {
             fun2();//exception handled
         }
-        catch(Exception e){System.out.println("Exception handled");
-    }}
-      public static void main(String args[]){
+        catch(Exception e)
+        {
+            System.out.println("Exception handled");
+    }
+    }
+      public static void main(String args[])
+      {
           exception_prpogation obj =new exception_prpogation();
           obj.fun3();
           System.out.println("rest of the code");
-      } 
-    
+      }  
 }
