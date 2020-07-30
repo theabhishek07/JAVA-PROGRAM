f2372fefbad3be2f0f0bd8fa01ddecdbd2a6cffd
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-07-31 00:35:54 +0530

    Update input_output#6.java

--- input_output#6.java
+++ input_output#6.java
@@ -1,6 +1,8 @@
 import java.io.*;
-public class reader_example {
-    public static void main(String[] args) {
+public class reader_example 
+{
+    public static void main(String[] args) 
+    {
         try{
             Reader reader=new FileReader("C:\\Users\\ABHISHEK\\Documents\\NetBeansProjects\\practice\\target\\classes\\practice_java\\test.txt");
             int data=reader.read();
