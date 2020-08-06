8da025853b1626901dc05f04ed47c250090c9b4a
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-08-07 00:25:26 +0530

    Update for_loop.java

--- for_loop.java
+++ for_loop.java
@@ -1,5 +1,7 @@
-public class for_loop {
-    public static void main(String[] args) {
+public class for_loop 
+{
+    public static void main(String[] args) 
+    {
         int a = 7;
         for(int i=1 ;i<a; i++){
         for(int j=a; j>=i;j--){
