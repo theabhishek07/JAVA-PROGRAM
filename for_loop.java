443af6263022332d65fab312f027b81039e09a80
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-08-08 00:00:33 +0530

    Update for_loop.java

--- for_loop.java
+++ for_loop.java
@@ -5,8 +5,7 @@ public class for_loop
         int a = 7;
         for(int i=1 ;i<a; i++){
         for(int j=a; j>=i;j--){
-                System.out.println("@");
-            }
+                System.out.println("@");     }
         System.out.println();
     }
     }
