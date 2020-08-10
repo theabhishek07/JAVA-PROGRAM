549cec888394e51864ea4cd21e13fa5067fe431d
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-08-10 14:52:43 +0530

    Update Inet.java

--- java networking/Inet.java
+++ java networking/Inet.java
@@ -1,9 +1,12 @@
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
-public class Inet {
-    public static void main(String[] args)  {
-        try{
+public class Inet 
+{
+    public static void main(String[] args) 
+    {
+        try
+        {
               InetAddress ip= InetAddress.getByName("www.github.com");     
               System.out.println("host name: "+ ip.getHostName());
               System.out.println("ip address: "+ ip.getHostAddress());
