215a58a2e8b530e0e84c984dfcd569df6db20759
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-08-15 01:04:08 +0530

    Update thread4.java

--- thread4.java
+++ thread4.java
@@ -1,8 +1,11 @@
-public class thread4 extends Thread{
-    public void run(){
+public class thread4 extends Thread
+{
+    public void run()
+    {
         System.out.println("running the thread");
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         thread4 t1=new thread4();
         thread4 t2=new thread4();
         System.out.println("thread 1 name by default is:"+ t1.getName());
