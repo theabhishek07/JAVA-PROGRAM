c11de37d3ddf7cad8c9ac1e72aad86db218a3437
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-08-16 00:30:47 +0530

    Update URL_connection.java

--- java networking/URL_connection.java
+++ java networking/URL_connection.java
@@ -1,7 +1,9 @@
 import java.io.*;
 import java.net.*;
-public class URL_connection {
-    public static void main(String[] args) {
+public class URL_connection 
+{
+    public static void main(String[] args)
+    {
         try{
             URL url=new URL("https://github.com/theabhishek07/JAVA-PROGRAM/tree/master/java%20networking");
             URLConnection cn= url.openConnection();
@@ -11,7 +13,9 @@ public class URL_connection {
                 System.out.println((char)i);//printing word by word
             }
             
-        }catch(Exception e){
+        }
+        catch(Exception e)
+        {
             System.out.println(e);
         }
     }
