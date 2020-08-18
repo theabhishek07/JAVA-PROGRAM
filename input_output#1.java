1b06ce72665dd7e81717d3edab3b50b7e1cce56e
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-08-18 23:30:26 +0530

    Update input_output#1.java

--- input_output#1.java
+++ input_output#1.java
@@ -1,8 +1,11 @@
 import java.io.FileOutputStream;
 import java.io.IOException;
-public class file_output {
-    public static void main(String[] args) {
-        try{
+public class file_output 
+{
+    public static void main(String[] args) 
+    {
+        try
+        {
             FileOutputStream fout= new FileOutputStream("C:\\Users\\ABHISHEK\\Documents\\NetBeansProjects\\practice\\target\\classes\\practice_java\\test.txt");
             String s="This a is java guide";
             byte b[]=s.getBytes();
