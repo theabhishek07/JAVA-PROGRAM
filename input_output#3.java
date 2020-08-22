09649d945441e0a7c1179ccb744dc23672c45c10
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-08-22 23:40:02 +0530

    Update input_output#3.java

--- input_output#3.java
+++ input_output#3.java
@@ -1,6 +1,8 @@
 import java.io.*;
-public class buffred_outputStream {
-    public static void main(String[] args) throws Exception {
+public class buffred_outputStream
+{
+    public static void main(String[] args) throws Exception
+    {
         FileOutputStream fout=new FileOutputStream("C:\\Users\\ABHISHEK\\Documents\\NetBeansProjects\\practice\\target\\classes\\practice_java\\test.txt");
         BufferedOutputStream bout=new BufferedOutputStream(fout);
       byte b[]={75,76,77,78,79,80};
@@ -8,7 +10,5 @@ public class buffred_outputStream {
       bout.flush();
       fout.close();
       bout.close();
-    }
-    
-    
+    }  
 }
