596f6093defc04a66cb1b838c50a830591c32b98
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-08-28 02:35:14 +0530

    Update overriding.java

--- overriding.java
+++ overriding.java
@@ -1,13 +1,17 @@
-class over{
+class over
+{
     void fun(){
         System.out.println("2020 over");
     }
 }
-public class overriding extends over {
-    void fun(){
+public class overriding extends over
+{
+    void fun()
+    {
         System.out.println("welcome COVID-19");
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         overriding obj=new overriding();
         over obj1 =new over();
         obj.fun();
