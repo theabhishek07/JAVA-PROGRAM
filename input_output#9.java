4a67cdc5cdbc4726efe576e7bd87a7e93b5176c9
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-08-29 22:43:18 +0530

    Update input_output#9.java

--- input_output#9.java
+++ input_output#9.java
@@ -1,6 +1,8 @@
 import java.io.*;
-public class buffered_reader {
-    public static void main(String[] args) throws Exception {
+public class buffered_reader 
+{
+    public static void main(String[] args) throws Exception
+    {
         FileReader rd=new FileReader("C:\\Users\\ABHISHEK\\Documents\\NetBeansProjects\\practice\\target\\classes\\practice_java\\test.txt");
         BufferedReader br= new BufferedReader(rd);
          int i;
