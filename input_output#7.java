95f8f7ef1cf032884fce87b05226898c3883b033
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-09-01 00:13:06 +0530

    Update input_output#7.java

--- input_output#7.java
+++ input_output#7.java
@@ -1,7 +1,10 @@
 import java.io.*;
-public class writer_example {
-    public static void main(String[] args) {
-        try{
+public class writer_example
+{
+    public static void main(String[] args) 
+    {
+        try
+        {
             Writer w=new FileWriter("C:\\Users\\ABHISHEK\\Documents\\NetBeansProjects\\practice\\target\\classes\\practice_java\\test.txt");
             String content="javajavajava";
             w.write(content);
@@ -9,7 +12,8 @@ public class writer_example {
             System.out.println("done");
             
         }
-        catch(Exception e){
+        catch(Exception e)
+        {
             System.out.println(e);
         }
     }
