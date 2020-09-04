3a612f18609703df96399a7b77e4ad09bb7ee483
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-09-04 23:55:52 +0530

    Update input_output#8.java

--- input_output#8.java
+++ input_output#8.java
@@ -1,6 +1,8 @@
 import java.io.*;
-public class buffered_writer {
-    public static void main(String[] args) throws Exception{
+public class buffered_writer
+{
+    public static void main(String[] args) throws Exception
+    {
         FileWriter writerss=new FileWriter("C:\\Users\\ABHISHEK\\Documents\\NetBeansProjects\\practice\\target\\classes\\practice_java\\test.txt");
         BufferedWriter buff=new BufferedWriter(writerss);
         buff.write("HELLO CODERS");
