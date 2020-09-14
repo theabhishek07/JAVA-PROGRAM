c3129383d8ec508529cfd12d040f7324d61b6537
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-09-14 23:32:44 +0530

    Update multidimensional.java

--- multidimensional.java
+++ multidimensional.java
@@ -1,5 +1,7 @@
-public class multidimensional {
-    public static void main(String[] args) {
+public class multidimensional
+{
+    public static void main(String[] args)
+    {
     int arr[][]={{1,2,3},{3,4,5},{4,5,6}};
     for(int i=0;i<3;i++){
         for(int j=0; j<3;j++){
