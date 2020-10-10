343e2c31a5ef43735db99d7ac9777cbed9d1a701
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-10-10 23:54:46 +0530

    Update object2.java

--- object2.java
+++ object2.java
@@ -1,18 +1,22 @@
-class student1{
+class student1
+{
     int roll;
     String subject;
-    void insert (int r, String s){
+    void insert (int r, String s)
+    {
         roll=r;
         subject=s;
     }
-    void display(){
+    void display()
+    {
         System.out.println(roll+ " "+subject);
     }
     
 }
 
 public class object2 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         student1 s1 =new student1();
         student1 s2=new student1 ();
         s1.insert(106,"maths");
