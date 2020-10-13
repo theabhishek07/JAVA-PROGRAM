ec4da876b268a893bf36e280880dc49722f2649d
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-10-13 23:37:12 +0530

    Update operator.java

--- operator.java
+++ operator.java
@@ -1,5 +1,7 @@
-public class operator {
-    public static void main(String[] args) {
+public class operator
+{
+    public static void main(String[] args)
+    {
         int a = 10 , b = 20 , c = 15 , d = 12 ;
         System.out.println(++a);// pre-increment
          System.out.println(b++);// post-increment
