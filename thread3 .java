c1dec370833b30a8f403991456d47f55654a2fa5
ABHISHEK SHARMA <asharma0269@gmail.com>
2020-12-11 22:31:46 +0530

    Update thread3 .java

--- thread3 .java
+++ thread3 .java
@@ -1,21 +1,29 @@
-class thread3 extends Thread{  
- public void run(){  
-  for(int i=1;i<=10;i++){  
-   try{  
+class thread3 extends Thread
+{  
+ public void run()
+ {  
+  for(int i=1;i<=10;i++)
+  {  
+   try
+   {  
     Thread.sleep(500);  
-   }catch(Exception e){System.out.println("hehe");}  
+   }
+   catch(Exception e){System.out.println("hehe");
+   }  
   System.out.println(i);  
   }  
  }  
-public static void main(String args[]){  
+public static void main(String args[])
+{  
  thread3 t1=new thread3();  
  thread3 t2=new thread3();
  thread3 t3=new thread3();
  t1.start();  
- try{  
+ try
+ {  
   t1.join();  
- }catch(Exception e){System.out.println(e);}  
-  
+ }catch(Exception e){System.out.println(e);
+ }
  t2.start();  
  t3.start();  
  }  
